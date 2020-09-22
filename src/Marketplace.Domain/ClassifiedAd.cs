0939f18acbe78368efca7fa101c6a6bbef8f15fc
Aymeric DE MOURA <ademoura@elcia.com>
2020-09-22 15:37:59 +0200

    Add first ValueObject

--- src/Marketplace.Domain/ClassifiedAd.cs
+++ src/Marketplace.Domain/ClassifiedAd.cs
@@ -6,20 +6,21 @@ namespace Marketplace.Domain
     {
         public Guid Id { get; private set; }
 
-        public ClassifiedAd(Guid id)
+        public ClassifiedAd(Guid id, UserId ownerId)
         {
             if (id == default)
                 throw new ArgumentException(
                     "Identity must be specified", nameof(id));
 
             Id = id;
+            _ownerId = ownerId;
         }
 
         public void SetTitle(string title) => _title = title;
         public void UpdateText(string text) => _text = text;
         public void UpdatePrice(decimal price) => _price = price;
 
-        private Guid _ownerId;
+        private UserId _ownerId;
         private string _title;
         private string _text;
         private decimal _price;
