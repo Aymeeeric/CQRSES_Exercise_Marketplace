028f6b3812558a43ed2370391c096a9d51f69850
Aymeric DE MOURA <ademoura@elcia.com>
2020-09-22 16:27:12 +0200

    Implement Base value

--- src/Marketplace.Domain/Money.cs
+++ src/Marketplace.Domain/Money.cs
@@ -1,5 +1,4 @@
 ﻿using Marketplace.Framework;
-using System;
 
 namespace Marketplace.Domain
 {
@@ -8,5 +7,17 @@ namespace Marketplace.Domain
         public decimal Amount { get; }
 
         public Money(decimal amount) => Amount = amount;
+
+        public Money Add(Money summand) =>
+            new Money(Amount + summand.Amount);
+
+        public Money Subtract(Money subtrahend) =>
+            new Money(Amount - subtrahend.Amount);
+
+        public static Money operator +(
+            Money summand1, Money summand2) => summand1.Add(summand2);
+
+        public static Money operator -(
+            Money minuend, Money subtrahend) => minuend.Subtract(subtrahend);
     }
 }
